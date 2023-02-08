7305261b467730ee2c2daa065fba8b7ea978f78e
RosianuRobert <94984223+RobertKeanu@users.noreply.github.com>
2023-02-08 18:31:49 +0200

    be done (program mistakes)

--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@ using System.Text;
 using Microsoft.OpenApi.Models;
 using ASP_Project.Helper;
 using ASP_Project.Helper.Middleware;
+using Swashbuckle.AspNetCore.Filters;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,6 +26,33 @@ builder.Services.AddRepo();
 builder.Services.AddServices();
 builder.Services.AddUtils();
 builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Description = "Standard auth header using bearer",
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
+
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
+                .GetBytes(builder.Configuration.GetSection("AppSettings:JwtSecret").Value!)),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
 
 //Services
 
